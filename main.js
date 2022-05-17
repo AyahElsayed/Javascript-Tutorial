fa1f2cf29132d8888ef324a727678748b7e692df
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-05-17 21:33:49 +0200

    strings

--- main.js
+++ main.js
@@ -28,3 +28,44 @@ console.log(typeof String(10)) //string
 // console.log(typeof 100.toString()) //error
 console.log(typeof (100).toString()) //string
 console.log(typeof 100..toString()) //string
+
+// ***************************************
+// strings
+let name = 'ali'
+console.log(name.repeat(2)) // aliali
+console.log(name.length) // 3
+
+console.log(name[2]) // i
+console.log(name.charAt(2)) //i
+
+console.log(name[10]) // undefined
+console.log(name.charAt(10)) // no output
+
+console.log(name.indexOf('l')) //1
+
+let x = 'I love javaScript'
+console.log(x.slice()) //I love javaScript
+console.log(x.slice(2)) // love javaScript
+console.log(x.slice(0,4)) //I lo
+
+console.log(x.split()) // ['I love javaScript']
+console.log(x.split('')) // ['I', ' ', 'l', 'o', 'v', 'e', ' ', 'j', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']
+console.log(x.split(' ')) // ['I', 'love', 'javaScript']
+console.log(x.split(' ', 2)) // ['I', 'love']
+
+// *************
+// cut stings 
+console.log(x.slice()) //I love javaScript
+console.log(x.substring()) //I love javaScript
+console.log(x.substr()) //I love javaScript
+
+console.log(x.slice(2,6)) //love
+console.log(x.substring(2,6)) //love
+console.log(x.substr(2,6)) //love j
+
+console.log(x.indexOf('a',9)) //10
+console.log(x.indexOf('z',9)) //-1 not found
+console.log(x.lastIndexOf('a',9)) //8
+console.log(x.lastIndexOf('z',9)) //-1 not found
+console.log(x.includes('a',9)) //true
+
