bf48ad463b282ef453027bebe0d4840882e86081
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-05-17 13:07:13 +0200

    convert strings to numbers

---
+++ main.js
@@ -0,0 +1,22 @@
+// convert strings to numbers
+
+console.log('5' + '4') // 54 conct
+console.log('5' - '4') // 1
+console.log('5' * '4') // 20
+console.log('5' / '4') // 1.25
+console.log('5' ** '4') // 625
+
+console.log('5' / 'ahmed') // Nan / convert ahmed to number
+
+console.log(  +'5' +  +'4'  ) // 9 / when add + before the number 
+// js convert conct to add
+
+
+console.log( typeof +'5') // number
+
+console.log(+'4') // 4 number
+console.log(+'4 Aya') // NaN
+console.log(parseInt('4 Aya')) //4 
+console.log(parseInt('ghg 4 Aya')) //NaN
+console.log(parseInt('4.5')) //4
+console.log(parseFloat('4.5')) //4.5
